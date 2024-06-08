62e94913d20bb1705581492b5bba54dcad9d2ac8
MajeFlz <42269070+MajeFlz@users.noreply.github.com>
2024-06-08 20:20:17 +0500

    Update homework.groovy

--- Jenkinsfile/homework.groovy
+++ Jenkinsfile/homework.groovy
@@ -7,5 +7,26 @@ pipeline {
                 sh 'df -h'
             }
         }
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('Performance') {
+            steps {
+                plot {
+                    title 'Performance'
+                    xaxis {
+                        label 'Time'
+                    }
+                    yaxis {
+                        label 'Response Time'
+                    }
+                    series {
+                        file 'performance-data.csv'
+                    }
+                }
+            }
+        }
     }
 }
