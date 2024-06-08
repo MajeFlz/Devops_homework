a1e3953bdf95c2d50986dd28334dd42bce726442
MajeFlz <42269070+MajeFlz@users.noreply.github.com>
2024-06-08 18:58:12 +0500

    Update homework.groovy

--- Jenkinsfile/homework.groovy
+++ Jenkinsfile/homework.groovy
@@ -4,6 +4,7 @@ pipeline {
     stages {
         stage('Информация о дисках') {
             steps {
+                git url: 'https://github.com/MajeFlz/jenkins_homework', credentialsId: 'belous_github'
                 sh 'df -h --output=source,fstype,size,used,avail,pcent,targets'
             }
         }
