09bc352996a46bf0b0ac7878891c1b68dbbee1b2
MajeFlz <42269070+MajeFlz@users.noreply.github.com>
2024-06-08 20:14:40 +0500

    Update homework.groovy

--- Jenkinsfile/homework.groovy
+++ Jenkinsfile/homework.groovy
@@ -7,14 +7,5 @@ pipeline {
                 sh 'df -h'
             }
         }
-       
-        stage('Build and deploy') {
-            steps {
-                dir('../docker') {
-                    sh 'docker run --rm -v $PWD:/app -w /app docker:latest docker build -t my-image.'
-                    sh 'docker run --rm -v $PWD:/app -w /app docker:latest docker-compose up -d'
-                }
-            }
-        }
     }
 }
