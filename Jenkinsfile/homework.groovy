1aa8297e1df1ca7272f8306097f4020d0bbf8bf0
MajeFlz <42269070+MajeFlz@users.noreply.github.com>
2024-06-08 19:59:35 +0500

    Update homework.groovy

--- Jenkinsfile/homework.groovy
+++ Jenkinsfile/homework.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Информация о дисках') {
             steps {
-                sh 'df -h --output=size'
+                sh 'df -h'
             }
         }
         stage('Докер') {
