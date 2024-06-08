43a27037d19c2b9d1d4b73eb16e6105fce2b97f9
MajeFlz <42269070+MajeFlz@users.noreply.github.com>
2024-06-08 20:23:03 +0500

    Update homework.groovy

--- Jenkinsfile/homework.groovy
+++ Jenkinsfile/homework.groovy
@@ -7,27 +7,5 @@ pipeline {
                 sh 'df -h'
             }
         }
-        stage('Build') {
-            steps {
-                sh 'apt-get update && sudo apt-get install -y maven' // Install Maven on the agent
-                sh 'mvn clean package'
-            }
-        }
-        stage('Performance') {
-            steps {
-                plot {
-                    title 'Performance'
-                    xaxis {
-                        label 'Time'
-                    }
-                    yaxis {
-                        label 'Response Time'
-                    }
-                    series {
-                        file 'performance-data.csv'
-                    }
-                }
-            }
-        }
     }
 }
